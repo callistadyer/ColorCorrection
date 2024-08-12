071a3816d778625f2b96d615e54c23e95828827e
callistadyer <callistadyer@gmail.com>
2024-08-12 18:03:19 -0400

    attempting to render dichromat image

--- tutorials/t_renderHyperspectralImage.m
+++ tutorials/t_renderHyperspectralImage.m
@@ -41,7 +41,7 @@ P_monitor = SplineSrf(displayGet(d,'wave'),displayGet(d,'spd'),wls);
 
 % Render lms image that a trichromat or dichromat sees
 lmsImageCalFormat = T_cones*hyperspectralImageCalFormat;
-RGBImage_trichromat = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n);
+[RGBImage_trichromat,scaleFactor]  = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n);
 
 
 % Testing different constant values of m-cone (or other missing cone)
@@ -55,7 +55,7 @@ l_cone = lmsImageCalFormat(1,:);
 m_cone = lmsImageCalFormat(2,:);
 s_cone = lmsImageCalFormat(3,:);
 
-renderType = 'Protanopia';
+renderType = 'Deuteranopia';
 if strcmp(renderType, 'Deuteranopia')
 
     % % use this to test different values for missing cone
@@ -66,17 +66,16 @@ if strcmp(renderType, 'Deuteranopia')
 
     % lmsImageCalFormat(2,:) = mean([lmsImageCalFormat(1,:),lmsImageCalFormat(3,:)],"all");
 
-lmsImageCalFormat(2,:)  = l_cone; % replace m cones with L cone mean
-RGBImage_dichromat      = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n);
-
+lmsImageCalFormat(2,:)  = l_cone; % replace M cones with L cone mean
 elseif strcmp(renderType, 'Protanopia')
-lmsImageCalFormat(1,:) = m_cone;   
-RGBImage_dichromat      = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n);
-
+lmsImageCalFormat(1,:)  = m_cone; % replace L cones with M cone mean
 elseif strcmp(renderType, 'Tritanopia')
-lmsImageCalFormat(3,:) = mean([lmsImageCalFormat(1,:),lmsImageCalFormat(2,:)],"all");    
-end
+% lmsImageCalFormat(3,:) = mean([lmsImageCalFormat(1,:),lmsImageCalFormat(2,:)],"all");    
+lmsImageCalFormat(3,:)  = m_cone; % replace S cones with M cone mean
+end  
 
+% get dichromat image
+[RGBImage_dichromat,scaleFactor]      = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n);
 
 % for i = 1:size(m_new,1) 
 % Show the image
@@ -94,12 +93,27 @@ title([renderType ' rendering']);
 
 % check by reversing RGB to LMS image
 LMSimage_trichromat = double(T_cones*hyperspectralImageCalFormat);
-LMSimage_dichromat  = double(RGB2LMSimg(RGBImage_dichromat,d,T_cones,P_monitor,m,n));
-sameLcone = isequal(LMSimage_trichromat(1,:),LMSimage_dichromat(1,:))
-sameMcone = isequal(LMSimage_trichromat(2,:),LMSimage_dichromat(2,:))
-sameScone = isequal(LMSimage_trichromat(3,:),LMSimage_dichromat(3,:))
+LMSimage_dichromat  = double(RGB2LMSimg(RGBImage_dichromat,d,T_cones,P_monitor,scaleFactor,m,n));
+sameLcone = isequal(LMSimage_trichromat(1,:),LMSimage_dichromat(1,:)); % do we get the same answer when we reverse the process?
+sameMcone = isequal(LMSimage_trichromat(2,:),LMSimage_dichromat(2,:));
+sameScone = isequal(LMSimage_trichromat(3,:),LMSimage_dichromat(3,:));
 
-function RGBImage = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n)
+figure('position',[743         503        1632         476]);
+subplot(1,3,1)
+scatter(LMSimage_trichromat(1,:),LMSimage_dichromat(1,:),'red','Marker','.','LineWidth',2)
+xlabel('desired L'); ylabel('recovered L'); title('L');
+axis square;
+subplot(1,3,2)
+scatter(LMSimage_trichromat(2,:),LMSimage_dichromat(2,:),'green','Marker','.','LineWidth',2)
+xlabel('desired M'); ylabel('recovered M'); title('M');
+axis square;
+subplot(1,3,3)
+scatter(LMSimage_trichromat(3,:),LMSimage_dichromat(3,:),'blue','Marker','.','LineWidth',2)
+xlabel('desired S'); ylabel('recovered S'); title('S');
+axis square;
+
+
+function [RGBImage,scaleFactor] = RGBimg(lmsImageCalFormat,d,T_cones,P_monitor,m,n)
 
 % Build matrix that goes from cones to monitor linear rgb
 M_rgb2cones = T_cones*P_monitor;
@@ -110,7 +124,8 @@ rgbImageCalFormat = M_cones2rgb*lmsImageCalFormat;
 rgbImage = CalFormatToImage(rgbImageCalFormat,m,n);
 
 % For right now, normalize so that maximum value in rgb is 1
-rgbImage = rgbImage/max(rgbImage(:));
+scaleFactor = max(rgbImage(:)); % save scale factor for later 
+rgbImage = rgbImage/scaleFactor;
 rgbImage(rgbImage < 0) = 0;
 
 % Gamma correct
@@ -120,14 +135,19 @@ RGBImage = rgb2dac(rgbImage,iGtable)/(2^displayGet(d,'dacsize')-1);
 
 end
 
-function lmsImageCalFormat = RGB2LMSimg(RGBImage,d,T_cones,P_monitor,m,n)
+function lmsImageCalFormat = RGB2LMSimg(RGBImage,d,T_cones,P_monitor,scaleFactor,m,n)
 
 % reverse the gamma correction
 gammaTable = displayGet(d,'gammatable');
-rgbImg = dac2rgb(RGBImage, gammaTable);
+rgbImg = dac2rgb(RGBImage, gammaTable)*(2^displayGet(d,'dacsize')-1);
 
+% undo the scaling 
+rgbImg = rgbImg * scaleFactor;
+
+% cal format
 rgbImageCalFormat = ImageToCalFormat(rgbImg);
 
+% lms image
 M_rgb2cones = T_cones*P_monitor;
 lmsImageCalFormat = M_rgb2cones * rgbImageCalFormat;
 
