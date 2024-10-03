0afe9542e28f0cf83657e63093868306cb5546cc
callistadyer <callistadyer@gmail.com>
2024-10-03 11:12:26 -0400

    Update T_EstObjectiveFunction.m

--- toolboxes/T_EstObjectiveFunction.m
+++ toolboxes/T_EstObjectiveFunction.m
@@ -1,4 +1,34 @@
 function obj = T_EstObjectiveFunction(kVec, D_mnew, T_mean, d, T_cones, P_monitor, m, n, bScale)
+% Objective function for choosing how to scale values after PCA to be in
+% similar range as LMS values
+%
+% Syntax:
+%   obj = T_EstObjectiveFunction(kVec, D_mnew, T_mean, d, T_cones, P_monitor, m, n, bScale)
+%
+% Description:
+%
+% Inputs:
+%   kVec:         - [1x3] vector. Initial guess for scalars on PCA outputs                    
+%   D_mnew:       - mean subtracted LMS values mapped onto two principle
+%                   components (and one is copied into the row of the
+%                   missing cone)
+%   T_mean        - mean values of original LMS
+%   d             - Struct.  Contains display information, displayCreate('LCD-Apple'); 
+%   T_cones       - [3xnWl]. Cone spectral sensitivities
+%   P_monitor     - [nWlx3]. Display primaries
+%   m             - Scalar.  Row dimension of image     
+%   n             - Scalar.  Column dimension of image    
+%   bScale:       - Boolean. Scale the image values into display range (1
+%                   or 0).  A good idea except for 'gray'.
+%
+% Outputs:
+%   obj:          - objective to minimize in fmincon
+%                   see colorCorrectionPCA.m
+%
+% Optional key/value pairs:
+%   None
+%
+
 
 % Make sure K hasn't entered the twilight zone
 if (any(isnan(kVec(:))))
