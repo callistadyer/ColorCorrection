95db8da6e04b3432a546b975d7da738859148df6
callistadyer <callistadyer@gmail.com>
2024-11-18 22:03:25 -0500

    Update decolorOptimize.m

--- toolboxes/DecolorizationAlgorithms/decolorOptimize.m
+++ toolboxes/DecolorizationAlgorithms/decolorOptimize.m
@@ -1,8 +1,34 @@
-function [] = decolorOptimize(data,numPCs)
-% MATLAB Script For Iterative PCA Using Fmincon Without The PCA Function
-% Input: A 3xN Matrix "data"
-% Output: Principal Components Stored In Matrix "PCs", Visualized Over Data
+function [PCs projected_data] = decolorOptimize(data,numPCs)
+% Uses fmincon to find the axis of projection that maximizes variance 
+%
+% Syntax:
+%   [PCs projected_data] = decolorOptimize(data,numPCs)
+%
+% Description:
+%
+% Inputs:
+%   data:   data to be projected
+%   numPCs: number of principal components to project onto
+%
+% Outputs:
+%   PCs:            principal components
+%   projected_data: data projected onto PCs
+%
+%
+% Optional key/value pairs:
+%   None
+%
+% Examples are included within the code
 
+% History
+%   09/14/2024  cmd  Initial go.
+%
+% Examples:
+%{
+
+%}
+
+% Sample data to see how code works
 if isempty(data)
     % Pretend data to visualize what the function does
     N = 100;                         % Number of points
@@ -12,52 +38,52 @@ if isempty(data)
     data = [x; y; z];
 end
 
-% Subtract the mean (center the data)
+% Subtract mean 
 mean_data = mean(data, 2);
 centered_data = data - mean_data; 
 
-% Initialize variables
 PCs = zeros(size(data, 1), numPCs); % Matrix to store principal components.
-
-% Options for fmincon
 options = optimoptions('fmincon', 'Algorithm', 'sqp', 'Display', 'iter');
 
-% Loop to extract principal components (loop for each numPCs)
+% Loop to get PCs
 for pcIdx = 1:numPCs
-    % Function to maximize variance
-    variance_function = @(X) -var(centered_data' * X); % Maximize variance = minimize negative variance.
+    % Maximize variance
+    variance = @(X) -var(centered_data' * X); % Maximize variance = minimize negative variance.
     
     % Nonlinear constraint function
     constraint_function = @(X) deal([], abs(sum(X.^2)) - 1); % Constraint ||X||^2 = 1.
     
     % Initial guess that satisfies constraint
-    X0 = [1; 0; 0]; % Impulse at first position
+    X0 = [1; 0; 0]; 
     
     % Find current principal component via minimizing the negative variance
-    [PC, ~] = fmincon(@(X) variance_function(X), X0, [], [], [], [], [], [], constraint_function, options);
+    [PC, ~] = fmincon(@(X) variance(X), X0, [], [], [], [], [], [], constraint_function, options);
     
     % Store PC
     PCs(:, pcIdx) = PC;
     
     % Remove current PC
-    projected_data = centered_data' * PC; % Projection onto current PC
-    centered_data = centered_data - PC * projected_data'; % Remove variance explained
+    projected_data(:,pcIdx)     = centered_data' * PC;                 % Projection onto current PC
+    variance_explained = PC * projected_data(:,pcIdx)';            
+    centered_data      = centered_data - variance_explained;   % Remove variance explained
 end
 
-disp('All Principal Components (column-wise):');
-disp(PCs);
+% disp('All Principal Components (column-wise):');
+% disp(PCs);
 
-% Plot the data and PCS
+% Plot original data
+% figure;
+% scatter3(data(1,:),data(2,:),data(3,:),'filled')
+
+% Plot the projected data and PCS
 figure;
 scatter3(centered_data(1, :), centered_data(2, :), centered_data(3, :), 'filled');
 hold on;
 
 % Plot PCs
-origin = mean_data; % Use the data mean as the origin for the PCs.
+origin = mean_data; % Data mean = the origin for the PCs
 for i = 1:numPCs
-    quiver3(origin(1), origin(2), origin(3), ...
-        PCs(1, i), PCs(2, i), PCs(3, i), ...
-        'LineWidth', 2, 'MaxHeadSize', 0.5); % Draw PC as an arrow.
+    quiver3(origin(1), origin(2), origin(3), PCs(1, i), PCs(2, i), PCs(3, i),'LineWidth', 2, 'MaxHeadSize', 0.5);
 end
 
 title('3D Data and Principal Components');
