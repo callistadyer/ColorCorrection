437f75c08929febda6d48a81b0bf81926c551827
callistadyer <callista@sas.upenn.edu>
2025-06-20 09:47:20 -0400

    Update similarityLMS.m

--- toolboxes/DecolorizationAlgorithms/similarityLMS.m
+++ toolboxes/DecolorizationAlgorithms/similarityLMS.m
@@ -54,25 +54,6 @@ switch (similarityType)
         % Squared chromaticity difference (preserve hue!)
         chroma_diff = (l_new - l_old).^2 + (m_new - m_old).^2;
 
-        % --- NEW LUMINANCE DEFINITION ---
-        % Weighted luminance from Stockman & Sharpe 10°
-        % k_L = 0.69283932;
-        % k_M = 0.34967567;
-        % k_S = 0;    % (ignored)
-        % 
-        % lum_old = k_L * L_old + k_M * M_old;
-        % lum_new = k_L * L_new + k_M * M_new;
-        % 
-        % % Squared luminance difference
-        % % luminance difference (L + M)
-        % % lum_old = L_old + M_old;
-        % % lum_new = L_new + M_new;
-        % lum_diff = (lum_new - lum_old).^2;
-        % 
-        % % weights for keeping it similar
-        % w_chroma = 1.0;
-        % w_luminance = 0.01;
-
         similarity = sum(w_chroma * chroma_diff + w_luminance * lum_diff);
 
     otherwise
